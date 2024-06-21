0ad9e06ba4b9a5f345ba7865c6641de6b8f31fac
Bryan De Jesús Rosa Tavarez <71520172+bryandejesusrt@users.noreply.github.com>
2024-06-21 05:03:28 +0000

    bryan changes

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Bryan De Jesus Rosa Tavarez",
+  title: "Full Stack Developer",
+  email: "bryandejesusrt@gmail.com",
+  gitHub: "bryandejesusrt",
+  instagram: "bryandejesusrt",
+  linkedIn: "bryandejesusrt",
+  medium: "bryandejesusrt",
+  twitter: "bryandejesusrt",
+  youTube: "bryandejesusrt",
 };
 
 const primaryColor = "#4E567E";
